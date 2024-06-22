93f0fea924baa243c1c2b8d2691cb8ab5f259a78
Mihaylo Markov Korunoski <mihajlo_korunoski@icloud.com>
2024-06-22 15:48:26 +0200

    Adding first bash script

---
+++ handling_numbers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+num1=10
+num2=20
+
+sum=$((num1 + num2))
+difference=$((num1 -num2))
+product=$((num1 * num2))
+quotient=$((num1 / num2))
+remaider=$((num1 % num2))
+
+echo "Sum: $sum"
+echo "Difference: $difference"
+echo "Product: $product"
+echo "Quotient: $quotient"
+echo "Remainder: $remaider"
+
